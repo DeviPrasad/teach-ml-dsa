67e8543a5c93eba0e1c2f10d5c7f1099c889a7c1
Devi Prasad <dp@web3pleb.org>
2025-10-24 16:20:43 +0530

    Rename tests; add a dry test.

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,15 @@ mod ntt;
 fn main() {
     let _ = keypair::key_gen().unwrap();
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::keypair;
+
+    #[test]
+    fn dry_2k_keygen_test() {
+        for _ in 0..2048 {
+            let _ = keypair::key_gen().unwrap();
+        }
+    }
+}
